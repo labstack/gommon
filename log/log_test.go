f987e5d273f1920cc6bbe871be7328038fb782f3
Vishal Rana <vr@labstack.com>
2015-11-22 12:15:40 -0800

    Update log_test.go

--- log/log_test.go
+++ log/log_test.go
@@ -3,21 +3,19 @@ package log
 import "testing"
 
 func TestLog(t *testing.T) {
-	// l := New("test")
-	// b := new(bytes.Buffer)
-	// l.SetOutput(b)
-	// test(l, TRACE, t)
-	// assert.Contains(t, b.String(), "trace")
-	// assert.Contains(t, b.String(), "fatal")
-	//
-	// b.Reset()
-	// SetOutput(b)
-	// test(global, NOTICE, t)
-	// assert.NotContains(t, b.String(), "info")
-	// assert.Contains(t, b.String(), "notice")
-	// assert.Contains(t, b.String(), "fatal")
-
-	test(global, 8, t)
+	l := New("test")
+	b := new(bytes.Buffer)
+	l.SetOutput(b)
+	test(l, TRACE, t)
+	assert.Contains(t, b.String(), "trace")
+	assert.Contains(t, b.String(), "fatal")
+	
+	b.Reset()
+	SetOutput(b)
+	test(global, NOTICE, t)
+	assert.NotContains(t, b.String(), "info")
+	assert.Contains(t, b.String(), "notice")
+	assert.Contains(t, b.String(), "fatal")
 }
 
 func test(l *Logger, v Level, t *testing.T) {
