698b50fc0fdaf17a24487c07e5f40eb291710c07
Gary Sentosa <gary.sentosa@gmail.com>
2023-08-12 09:58:38 +0900

    feat: use local seed instead global one

--- random/random.go
+++ random/random.go
@@ -3,13 +3,29 @@ package random
 import (
 	"math/rand"
 	"strings"
+	"sync"
 	"time"
 )
 
 type (
 	Random struct {
+		lock sync.Mutex
+		src  rand.Source
 	}
 )
+
+func (r *Random) Int63() (n int64) {
+	r.lock.Lock()
+	n = r.src.Int63()
+	r.lock.Unlock()
+	return
+}
+
+func (r *Random) Seed(seed int64) {
+	r.lock.Lock()
+	r.src.Seed(seed)
+	r.lock.Unlock()
+}
 
 // Charsets
 const (
@@ -27,8 +43,8 @@ var (
 )
 
 func New() *Random {
-	rand.Seed(time.Now().UnixNano())
-	return new(Random)
+	src := rand.NewSource(time.Now().UnixNano())
+	return &Random{src: src}
 }
 
 func (r *Random) String(length uint8, charsets ...string) string {
@@ -38,7 +54,7 @@ func (r *Random) String(length uint8, charsets ...string) string {
 	}
 	b := make([]byte, length)
 	for i := range b {
-		b[i] = charset[rand.Int63()%int64(len(charset))]
+		b[i] = charset[r.Int63()%int64(len(charset))]
 	}
 	return string(b)
 }
--- random/random_test.go
+++ random/random_test.go
@@ -1,6 +1,7 @@
 package random
 
 import (
+	"math/rand"
 	"regexp"
 	"testing"
 
@@ -11,6 +12,19 @@ func Test(t *testing.T) {
 	assert.Len(t, String(32), 32)
 	r := New()
 	assert.Regexp(t, regexp.MustCompile("[0-9]+$"), r.String(8, Numeric))
+}
+
+func TestRandomString(t *testing.T) {
+	r := New()
+	// overwrite initialized Source by New()
+	rand.Seed(1)
+
+	got := r.String(32, Alphanumeric)
+	str := "onrHFkSuohOf8IvDIjS5HUHTGvw8q5mt"
+
+	if got == str {
+		t.Errorf("want random string; got: %s\n", got)
+	}
 }
 
 func BenchmarkRandomString(b *testing.B) {
