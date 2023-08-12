1a26b104fa799f59597434f1f3fa314aa4ecbfca
Gary Sentosa <gary.sentosa@gmail.com>
2023-08-12 23:55:53 +0900

    Revert "test: remove random string test"
    This reverts commit 213dc718c3800e6a449c0c78e167d0ce363dd5fe.

--- random/random_test.go
+++ random/random_test.go
@@ -1,6 +1,7 @@
 package random
 
 import (
+	"math/rand"
 	"regexp"
 	"testing"
 
@@ -10,8 +11,20 @@ import (
 func Test(t *testing.T) {
 	assert.Len(t, String(32), 32)
 	r := New()
-	want := r.String(8, Numeric)
-	assert.Regexp(t, regexp.MustCompile("[0-9]+$"), want)
+	assert.Regexp(t, regexp.MustCompile("[0-9]+$"), r.String(8, Numeric))
+}
+
+func TestRandomString(t *testing.T) {
+	r := New()
+	// overwrite initialized Source by New()
+	rand.Seed(1)
+
+	got := r.String(32, Alphanumeric)
+	str := "onrHFkSuohOf8IvDIjS5HUHTGvw8q5mt"
+
+	if got == str {
+		t.Errorf("want random string; got: %s\n", got)
+	}
 }
 
 func BenchmarkRandomString(b *testing.B) {
